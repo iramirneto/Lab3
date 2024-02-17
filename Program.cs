48a4cc0f2319f8d48d4961d7bf4035edf79c5cea
Iramir Neto <70657151+iramirneto@users.noreply.github.com>
2024-02-17 15:43:20 -0500

    Average.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Iramir Neto");
+namespace MyAveLib;
+
+public class Average
+{
+    public static double Ave(double num1, double num2, double num3) {
+        double ave;
+        ave = (num1+num2+num3)/3.0;
+
+        Console.WriteLine("The average is " + ave);
+        
+        return ave;
+    }
+    
+}  
